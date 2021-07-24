e6cb86f0cc12196281aa8dc95c5f8d0033f58f12
name <varunbrahmane30@gmail.com>
2021-07-24 10:49:45 +0530

    [varun] added: Hash table main program.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day15_HashTableProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash table problem To print the Frequency of Words.");
+        }
+    }
+}
